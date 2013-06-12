03dd1bf9ca9e1f53eaff8d8a172b15eecb5f8dee
Michael Davidsaver <mdavidsaver@bnl.gov>
2013-06-12 15:14:23 -0400

    fix python 2.7

--- carchive/__init__.py
+++ carchive/__init__.py
@@ -18,6 +18,8 @@ if not hasattr(logging, 'NullHandler'):
     class NullHandler(logging.Handler):
         def emit(self, record):
             pass
+else:
+    NullHandler = logging.NullHandler
 
 __h=NullHandler()
 logging.getLogger("carchive").addHandler(__h)
