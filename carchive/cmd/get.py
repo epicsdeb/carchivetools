d75df7fb112470ee9f3ade332b500cb7e7103704
Michael Davidsaver <mdavidsaver@bnl.gov>
2013-12-22 09:46:27 -0500

    add option to print time as posix floats

--- carchive/cmd/get.py
+++ carchive/cmd/get.py
@@ -7,26 +7,43 @@ from twisted.internet import defer
 
 from carchive.date import makeTimeInterval, makeTime
 
-def printData(data, meta, archive):
-    assert len(meta)==data.shape[0]
-    
-    if data.shape[1] == 1: # scalar
-        data = data.reshape((data.shape[0],))
-        for M,D in zip(meta,data):
-            print makeTime((M['sec'],int(M['ns']))), D,
-            print archive.severity(M['severity']),
-            print archive.status(M['status'])
+class Printer(object):
+    def __init__(self, opt):
+        if not opt.timefmt or opt.timefmt=='string':
+            self.timefmt = self.timestring
+        elif opt.timefmt=='posix':
+            self.timefmt = self.timeposix
+        else:
+            raise ValueError("Invalid time format %s"%opt.timefmt)
 
-    else: # waveform
-        for i,M in enumerate(meta):
-            print makeTime((M['sec'],int(M['ns']))),
-            print archive.severity(M['severity']),
-            print archive.status(M['status']),
-            print data[i,:].tolist()
+    def timeposix(self, (sec,ns)):
+        return sec+1e-9*ns
+
+    def timestring(self, ts):
+        return makeTime(ts)
+
+    def __call__(self, data, meta, archive):
+        assert len(meta)==data.shape[0]
+
+        if data.shape[1] == 1: # scalar
+            data = data.reshape((data.shape[0],))
+            for M,D in zip(meta,data):
+                print self.timefmt((M['sec'],int(M['ns']))), D,
+                print archive.severity(M['severity']),
+                print archive.status(M['status'])
+
+        else: # waveform
+            for i,M in enumerate(meta):
+                print self.timefmt((M['sec'],int(M['ns']))),
+                print archive.severity(M['severity']),
+                print archive.status(M['status']),
+                print data[i,:].tolist()
 
 @defer.inlineCallbacks
 def cmd(archive=None, opt=None, args=None, conf=None, **kws):
-    
+
+    printData = Printer(opt)
+
     archs=set()
     for ar in opt.archive:
         archs|=set(archive.archives(pattern=ar))
