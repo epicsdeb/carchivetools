300ac000745bb3257a7e8c2f602ca42c825aae22
Michael Davidsaver <mdavidsaver@bnl.gov>
2014-08-15 18:34:34 -0400

    wild2re

--- carchive/util.py
+++ carchive/util.py
@@ -1,3 +1,36 @@
 
+import re
+
 class HandledError(Exception):
     pass
+
+
+_wild = re.compile(r'(?:\\(.))|([*?])|([^*?\\]+)')
+
+def wild2re(pat):
+    """Translate a wildcard pattern into a regular expression
+    
+    >>> wild2re("hello")
+    'hello'
+    >>> wild2re("hello.")
+    'hello\\\\.'
+    >>> wild2re("he?lo.")
+    'he.lo\\\\.'
+    >>> wild2re(r"he?lo. wor\?d")
+    'he.lo\\\\.\\\\ wor\\\\?d'
+    >>> wild2re(r"hel*w\*rld")
+    'hel.*w\\\\*rld'
+    """
+    out=""
+    for esc, wc, txt in _wild.findall(pat):
+        if wc=='?':
+            out+='.'
+        elif wc=='*':
+            out+='.*'
+        else:
+            out+=re.escape(txt or esc)
+    return out
+
+if __name__=='__main__':
+    import doctest
+    doctest.testmod()
