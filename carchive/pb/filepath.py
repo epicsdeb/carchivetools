909ba9e8915db231058ee9679a89cd2af4d2bc03
bobnarj <jaka.bobnar@cosylab.com>
2014-11-11 22:30:40 +0100

    Added the pb installer to the setup.py. Updated the path for windows OS, which does not allow : in the file name.

--- carchive/pb/filepath.py
+++ carchive/pb/filepath.py
@@ -1,5 +1,6 @@
 import re
 import os
+import platform
 
 def get_dir_and_prefix(out_dir, delimiters, pv_name):
     regexPattern = '|'.join(map(re.escape, delimiters))
@@ -8,10 +9,16 @@ def get_dir_and_prefix(out_dir, delimiters, pv_name):
 
 def get_path_for_suffix(out_dir, delimiters, pv_name, time_suffix):
     dir_path, file_prefix = get_dir_and_prefix(out_dir, delimiters, pv_name)
-    return os.path.join(dir_path, '{}:{}.pb'.format(file_prefix, time_suffix))
+    name = '{}:{}.pb'
+    if platform.system() == 'Windows':
+        name = '{}@{}.pb'
+    return os.path.join(dir_path, name.format(file_prefix, time_suffix))
 
 def filter_filenames(names, file_prefix):
+    reg = '\\A{}:(.*)\\.pb\\Z';
+    if platform.system() == 'Windows':
+        reg = '\\A{}@(.*)\\.pb\\Z'
     for name in names:
-        m = re.match('\\A{}:(.*)\\.pb\\Z'.format(re.escape(file_prefix)), name)
+        m = re.match(reg.format(re.escape(file_prefix)), name)
         if m:
             yield m.group(1)
