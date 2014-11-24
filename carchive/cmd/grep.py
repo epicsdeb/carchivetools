7a10494dd4db262b7cb3ef9227f493b440b78562
Michael Davidsaver <mdavidsaver@bnl.gov>
2014-11-24 16:25:30 -0500

    argrep understands wildcards

--- carchive/cmd/grep.py
+++ carchive/cmd/grep.py
@@ -4,38 +4,28 @@ import logging
 _log = logging.getLogger("argrep")
 
 from twisted.internet import defer
+from ..date import makeTime
 
 @defer.inlineCallbacks
-def cmd(archive=None, opt=None, args=None, conf=None, **kws):
+def cmd(archive=None, opt=None, args=None, conf=None, breakDown=None, **kws):
     if len(args)==0:
         args=['.*']
 
-    _log.debug('Searching for: %s',args)
-
     archs=opt.archive
-    _log.info('Looking in: %s',', '.join(archs))
+    _log.info('Looking in: %s',', '.join(map(str,archs)))
 
-    res = {}
-
-    for pat in args:
-        S = yield archive.search(pattern=pat, archs=archs,
-                                 breakDown=opt.verbose>1)
-        res.update(S)
+    res = breakDown
 
     _log.debug('Found %d results',len(res))
 
     chs=res.keys()
     chs.sort()
     for c in chs:
-        if opt.verbose==0:
-            print c
-            continue
+        print c
+        if opt.verbose>0:
 
-        ranges=res[c]
-        if opt.verbose>1:
-            print c
+            ranges=res[c]
             for s,e,ar in ranges:
-                print ' ', s, ',', e, ',', archive.lookupArchive(ar)
+                print ' ', makeTime(s), ',', makeTime(e), ',', archive.lookupArchive(ar)
 
-        elif opt.verbose>0:
-            print ' ',ranges[0],',',ranges[1],',',c
+    yield defer.succeed(None)
