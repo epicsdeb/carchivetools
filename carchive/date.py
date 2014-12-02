f6d30721fcd3c8fa4fadcc17b02a1b81f79b9262
Michael Davidsaver <mdavidsaver@bnl.gov>
2014-12-01 19:22:56 -0500

    redo absolute date/time parsing

--- carchive/date.py
+++ carchive/date.py
@@ -5,14 +5,15 @@ Date string format
 Parsing of absolute and relative dates and times
 into datetime.datetime or datetime.timedelta instances
 
-Supported syntax:
+Supported string syntax:
 
   Absolute:
-    "day/month[/year] hour:min[:sec[.fraction]]"
-    "hour:min[:sec[.fraction]]"
-    "now"
+    "day/month[/year] hour:min[:sec[.fraction]][Z]"
+    "[year-]month-day hour:min[:sec[.fraction]][Z]"
+    "hour:min[:sec[.fraction]][Z]"
 
   Relative:
+    "now"
     "### UUU [### UUU ...]"
 
   where ### is a signed floating point number,
@@ -31,10 +32,10 @@ Supported syntax:
   eg: "-1.4 week 2 hours"
 """
 
-import datetime, time, re, sys
+import datetime, time, re, sys, calendar
 from collections import defaultdict
 
-__all__ = ["makeTime", "makeTimeInterval", 'timeTuple']
+__all__ = ["makeTime", "makeTimeInterval", 'timeTuple', 'isoString']
 
 # python provides no concrete implementations of tzinfo
 # and even if the zone database is available (pytz module)
@@ -42,18 +43,45 @@ __all__ = ["makeTime", "makeTimeInterval", 'timeTuple']
 # in a format which pytz understands.
 # I give up.  doctests will only work in US/Eastern
 
-# match absolute date plus time
-# DAY/MON/YEAR HOUR:MIN:SEC.FRAC
-_abs=re.compile(r"""
-  (?:
-      ([0-9]{1,2}) / ([0-9]{1,2}) # D/M
-        (?: / ([0-9]{1,4}) )? # Y
-  \s+
-  )? ([0-2]?[0-9]) : ([0-6]?[0-9]) # H:m
-        (?: : ([0-6]?[0-9]) # S
-            (?: . ([0-9]+) )? # F
-        )?
-""",re.X)
+# A guide to time format manipulations in Python
+# Formats: float, timetuple, and datetime
+#  float is always posix, timetuple and datetime are TZ specific.
+#  Timetuples track TZ implicitly.
+#  datetime may have explicit TZ, defaults to None (implicit TZ)
+#
+# Conversions:
+#   time.gmtime()     float -> timetuple UTC
+#   time.localtime()  float -> timetuple Local
+#   time.mktime()     timetuple Local -> float
+#   calendar.timegm() timetuple UTC -> float
+#
+#   datetime.fromtimestamp()    float -> datetime Local
+#   datetime.utcfromtimestamp() float -> datetime UTC
+#   datetime.timetuple()        datetime Local -> timetuple Local
+#   datetime.timetuple()        datetime UTC   -> timetuple UTC
+#   datetime.utctimetuple()     datetime Local -> timetuple Local (Never call this)
+#   datetime(*timetuple)        timetuple Local -> datetime Local
+#   datetime(*timetuple)        timetuple UTC   -> datetime UTC
+#
+# Note: none of the functions producing datatime objects set the TZ
+#
+
+# Match: H:M[:S[.F]]
+_tpat = r'(\d+):(\d+)(?::(\d+)(?:\.(\d+))?)?'
+
+# Match D/M[/Y]
+_dpat1= r'(\d+)/(\d+)(?:/(\d+))?'
+
+# Match [Y-]M-D
+_dpat2= r'(?:(\d+)-)?(\d+)-(\d+)?'
+
+_tzpat= r'\s*([Zz])?'
+
+# Match [D/M[/Y] ]H:M[:S[.F]][Z]
+_ts1 = re.compile(r'(?:%s[\sT]+)?%s%s'%(_dpat1, _tpat, _tzpat))
+
+# Match [[Y-]M-D ]H:M[:S[.F]][Z]
+_ts2 = re.compile(r'(?:%s[\sT]+)?%s%s'%(_dpat2, _tpat, _tzpat))
 
 # short hand and conversions for interval specifications
 _units={
@@ -90,8 +118,82 @@ if sys.version_info<(2,7):
 else:
     total_seconds = datetime.timedelta.total_seconds
 
+def _fromAbsString(intime, now):
+    """Parse the given string into a tuple of (sec, ns) or None if the format is wrong.
+    
+    Year, Month, Day, seconds, and fraction may be omitted.
+    
+    'now' is a float time used to fill in missing YMD components, SF are zeroed
+    
+    >>> _fromAbsString('2014-12-1 17:38:43.903230001', 5)
+    (1417473523, 903230001)
+    >>> _fromAbsString('2014-12-01  22:38:43.903230Z', 5)
+    (1417473523, 903230000)
+    >>> _fromAbsString('2014-12-01  22:38:43.903230z', 5)
+    (1417473523, 903230000)
+    >>> _fromAbsString('2014-12-01  22:38:43 Z', 5)
+    (1417473523, 0)
+    >>> 1417473523-43
+    1417473480
+    >>> _fromAbsString('2014-12-01  22:38Z', 1417473523)
+    (1417473480, 0)
+    >>> _fromAbsString('12-01 22:38:43Z', 1417473523)
+    (1417473523, 0)
+    >>> _fromAbsString('22:38:43Z', 1417473523)
+    (1417473523, 0)
+    >>> _fromAbsString('22:38Z', 1417473523)
+    (1417473480, 0)
+    >>>
+    >>> _fromAbsString('1/12/2014  22:38:43.903230Z', 5)
+    (1417473523, 903230000)
+    >>> _fromAbsString('1/12/2014  22:38:43Z', 1417473523)
+    (1417473523, 0)
+    >>> _fromAbsString('1/12  22:38Z', 1417473523)
+    (1417473480, 0)
+    >>> _fromAbsString('22:38Z', 1417473523)
+    (1417473480, 0)
+    >>>
+    >>> _fromAbsString('2014-12-01T22:38:43.903230Z', 5)
+    (1417473523, 903230000)
+    >>>
+    >>> _fromAbsString("-1 h", 5)
+    >>> _fromAbsString("Invalid", 5)
+    >>>
+    """
+
+    M = _ts1.match(intime)
+    if M is not None:
+        #day, mon, year, hour, min, sec, frac = M.group()
+        parts = list(M.groups())
+        parts[0], parts[2] = parts[2], parts[0] # swap year and day
+    else:
+        M = _ts2.match(intime)
+        if M is not None:
+            #year, mon, day, hour, min, sec, frac = M.group()
+            parts = list(M.groups())
+        else:
+            return None
+
+    # converter from timetuple to float
+    conv = calendar.timegm if parts[7] in ['z','Z'] else time.mktime
+    # converter from float to timetuple
+    rconv = time.gmtime if parts[7] in ['z','Z'] else time.localtime
+
+    D = rconv(now)[:3] + (0,0,0) # keep YMD, zero HMS
+
+    nsec = 0.0
+    if parts[6] is not None:
+        nsec = float('0.'+parts[6])*1e9 # fraction to nanoseconds
+
+    # if component is not None, convert to int(), otherwise use the default
+
+    parts = [int(v) if v else d for v,d in zip(parts[:6], D)]
+    parts += [0,0,-1] # disable DST compensation
+
+    return int(conv(parts)), int(nsec)
+
 def timeTuple(dt):
-    """Convert datetime object to (sec, nsec)
+    """Convert (local) datetime object to (sec, nsec)
 
     *sec* is POSIX seconds
 
@@ -108,21 +210,21 @@ def timeTuple(dt):
     return S,NS
 
 def isoString(dt):
-    """Convert a datetime object to a ISO 8601 UTC string representation
+    """Convert a (local) datetime object to a ISO 8601 UTC string representation
 
     eg. 2014-04-10T16:27:37.767454Z
 
     >>> import datetime
-    >>> now=datetime.datetime(2011, 3, 15, 13)
+    >>> now=datetime.datetime(2011, 3, 15, 13, 15, 14, 123000)
     >>> isoString(now)
-    '2011-03-15T17:00:00Z'
+    '2011-03-15T17:15:14.123000Z'
     """
     S, NS = timeTuple(dt)
-    udt = datetime.datetime.utcfromtimestamp(S).replace(microsecond=NS/1000)
+    udt = datetime.datetime.utcfromtimestamp(S+NS*1e-9)
     return udt.isoformat('T')+'Z'
 
 def makeTime(intime, now=None):
-    """Turn *intime* into datetime or timedelta
+    """Turn *intime* into (local) datetime or timedelta
 
     *intime* can be a tuple (sec,nsec), number, or string
 
@@ -150,13 +252,20 @@ def makeTime(intime, now=None):
     >>> makeTime( '12:01:14', now)
     datetime.datetime(2011, 3, 15, 12, 1, 14)
     >>> makeTime( '12:01:14.123456789', now)
+    datetime.datetime(2011, 3, 15, 12, 1, 14, 123457)
+    >>> makeTime( '12:01:14.123456389', now)
     datetime.datetime(2011, 3, 15, 12, 1, 14, 123456)
     >>> makeTime( '14/3 12:01', now)
     datetime.datetime(2011, 3, 14, 12, 1)
+    >>> makeTime( '14/3 12:01Z', now)
+    datetime.datetime(2011, 3, 14, 8, 1)
     >>> makeTime( '14/3 12:01:14', now)
     datetime.datetime(2011, 3, 14, 12, 1, 14)
     >>> makeTime( '14/3/2012 12:01:14.123456', now)
     datetime.datetime(2012, 3, 14, 12, 1, 14, 123456)
+    >>>
+    >>> makeTime('2014-12-01  22:38:43.903230Z', now)
+    datetime.datetime(2014, 12, 1, 17, 38, 43, 903230)
     >>>
     >>> now=datetime.datetime.now()
     >>>
@@ -219,30 +328,11 @@ def makeTime(intime, now=None):
     if intime=='now' or len(intime)==0:
         return now
 
-    S = intime.split('.')
-    if len(S)<=2:
-        try:
-            US = 0 if len(S)==1 else float('0.'+S[1])*1e6
-            T = time.strptime(S[0], '%Y-%m-%d %H:%M:%S')
-            return datetime.datetime(*(T[0:6]+(int(US),)))
-        except ValueError:
-            del S
+    R = _fromAbsString(intime, time.mktime(now.timetuple()))
+    if R is not None:
+        return datetime.datetime.fromtimestamp(R[0]+R[1]*1e-9)
 
-    M=_abs.match(intime)
-    if M is not None:
-        # take missing pieces from now
-        G=M.groups()[0:6]
-        G=G[2::-1]+G[3:] # swap day and year
-        G=zip(G, now.timetuple()[0:6])
-        G=[int(A) if A is not None else B for A,B in G]
-
-        US=int(float('0.'+(M.group(7) or '0'))*1e6)
-
-        Y, M, D, H, m, S = G
-        return datetime.datetime(year=Y, month=M, day=D, hour=H,
-                                 minute=m, second=S, microsecond=US,
-                                 tzinfo=tzinfo)
-
+    # treat as relative
     M=intime.split()
     if len(M)%2==1:
         raise ValueError('unexpect ending of \'%s\''%intime)
