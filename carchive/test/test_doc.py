455b3a8c6be511ae20d8785397152fc6ce1bbba3
Michael Davidsaver <mdavidsaver@gmail.com>
2022-01-02 10:17:01 -0800

    skip date doctests w/ 2.7
    too much trouble to support both...

--- carchive/test/test_doc.py
+++ carchive/test/test_doc.py
@@ -3,6 +3,11 @@ Copyright 2015 Brookhaven Science Assoc.
  as operator of Brookhaven National Lab.
 """
 
+import sys
 from .. import date, util, _conf
 
-__doctests__ = [date, util, _conf]
+__doctests__ = [util, _conf]
+if sys.version_info>=(3,0):
+    # TODO: differences in datetime.__repr__ make doctest compatibility difficult
+    #       should rewrite to unittest
+    __doctests__.append(date)
